b614147dbad8cd3a0b62ae75042323cd577ce58f
Ali Loudagh <63974953+tactics177@users.noreply.github.com>
2024-03-11 17:06:10 +0100

    Delete m1devefrei_g2_graphql directory

--- m1devefrei_g2_graphql/scripts/register.js
+++
@@ -1,9 +0,0 @@
-import {register} from "node:module";
-import {pathToFileURL} from "node:url";
-import {setUncaughtExceptionCaptureCallback} from "node:process"
-
-register("ts-node/esm", pathToFileURL("./"));
-setUncaughtExceptionCaptureCallback((err) => {
-  console.error(err);
-  process.exit(1);
-});
